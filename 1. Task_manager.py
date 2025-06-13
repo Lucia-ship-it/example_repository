0730ce8b4cb1c7468100a7cd247530e2d2b01a6f
ella <elalek@gmail.com>
2025-06-13 17:20:46 +0200

    zadanie 2 - pridanie ulohy

--- 1. Task_manager.py
+++ 1. Task_manager.py
@@ -1,3 +1,48 @@
+# ZADANIE
+# Každá funkce má svůj specifický úkol
+# Úkoly budou ukládány do seznamu ukoly = []
+
+
+
+#UKOL 2 - def pridat_ukol():
+# ok    volba 1 v hlavním menu
+# ok    nazev a popis noveho ukolu
+# ok    ulozit do zoznamu
+# ok    zobrazit nabidku hlavneho menu
+#!!!! osetrit zadanie prazdneho vstupu
+
+vsechny_ukoly = []
+def pridat_ukol():
+    
+    
+    while True:
+        nazev = input("\nZadejte název úkolu:").strip()
+        if nazev == "":
+            print("Vyplnenie je povinné")
+        else:
+            break
+
+    while True:
+        popis = input("Zadejte popis úkolu:").strip()
+        if popis == "":
+            print("Vyplnenie je povinné")
+        else:
+            break
+
+
+
+    novy_ukol = {
+        "název" : nazev,
+        "popis úkolu" : popis
+        }
+    vsechny_ukoly.append(novy_ukol)
+    print(f"Úkol {nazev} byl přidán.\n")
+
+#zadanie 1
+# UKOL 1 - Funkce hlavního menu
+# přidání, zobrazení a odstranění úkolu. 
+# neplatnou volbu, program ho upozorní a nechá uživatele opakovat znovu volbu.
+
 def hlavni_menu():
     print("Správce úkolů - Hlavní menu")
     print("1. Přidat nový úkol")
@@ -10,7 +55,7 @@ def hlavni_menu():
         print(vyber_cisla)
         if vyber_cisla == 1:
             print("Volba cislo 1")
-            break
+            pridat_ukol()
         elif vyber_cisla == 2:
             print("Volba cislo 2")
             break
@@ -25,3 +70,6 @@ def hlavni_menu():
             print(vyber_cisla)
 
 hlavni_menu()
+
+
+
