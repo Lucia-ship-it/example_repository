d5466965cc6bfca843dbfbd164b9ba27db0c80fd
ella <elalek@gmail.com>
2025-06-16 15:29:14 +0200

    osetrenie neciselnych vstupov pri - odstraneni ukolu

--- 1. Task_manager.py
+++ 1. Task_manager.py
@@ -24,14 +24,14 @@ vsechny_ukoly = [
 
 def pridat_ukol():
     while True:
-        nazev = input("\nZadejte název úkolu:").strip()
+        nazev = input("\nZadejte název úkolu: ").strip()
         if nazev == "":
             print("Vyplnenie je povinné")
         else:
             break
 
     while True:
-        popis = input("Zadejte popis úkolu:").strip()
+        popis = input("Zadejte popis úkolu: ").strip()
         if popis == "":
             print("Vyplnenie je povinné")
         else:
@@ -42,7 +42,7 @@ def pridat_ukol():
         "popis" : popis
         }
     vsechny_ukoly.append(novy_ukol)
-    print(f"Úkol {nazev} byl přidán.\n")
+    print(f"\nÚkol '{nazev}' byl přidán.\n")
 
 # ok    zadanie 2 - zobrazeni ukolu
 # ok    zobrazi vsechny ukoly v seznamu
@@ -54,44 +54,39 @@ def zobrazit_ukoly():
     print("\n\n")
     hlavni_menu()
 
-#zadanie 3 - odstranit ukol
+# ok    zadanie 3 - odstranit ukol
 # ok    umožnit zadat číslo úkolu, který chce odstranit
 # ok    platí volba 3 v hlavním menu
 # ok    je potřeba, aby uživatel viděl všechny uložené úkoly
-#při výběru neexistujícího úkolu byl upozorněn.
+# ok    při výběru neexistujícího úkolu byl upozorněn.
 # ok    program pokračuje dál nabídkou hlavního menu
 
-# UPRAVENY NADEJNY KOD: osetrit neciselne vstupy...:/
-# def odstranit_ukol():
+def odstranit_ukol():
+    #print(vsechny_ukoly)
+    for i, uloha in enumerate(vsechny_ukoly, start=1):
+        print(f"{i}. {uloha}")
 
-# zoznam = ["jablko","hruska","kiwi","melon"]
-# print(zoznam)
-
-# #enumetare priradi k polozkam zoznamu cislo
-
-# for i, uloha in enumerate(zoznam, start=1):
-#     print(f"{i}. {uloha}")
-
-# while True:
-#     vyber_cisla = int(input("Zadej číslo úkolu, který chceš smazat"))
-
-#     index_cisla=vyber_cisla - 1
-#     print(zoznam[index_cisla])
-
-#     if 0 <= index_cisla < len(zoznam):
-#         odstraneny_index = zoznam.pop(index_cisla)
-#         print(f"Úkol číslo {vyber_cisla} byl smazán")
-#         print(f"Smazali jste: {odstraneny_index}.")
-#         break
-#     else:
-#         print(f"V zozname sa nenachází úkol číslo {vyber_cisla}. Vyber číslo ze seznamu.")
+    while True:
+        try:
+            vyber_cisla = int(input("\nZadej číslo úkolu, který chceš smazat: \n"))
+            index_cisla = vyber_cisla - 1 
         
-#   print(zoznam)
+            if 0 <= index_cisla <= len(vsechny_ukoly):
+                odstraneny_index = vsechny_ukoly.pop(index_cisla) 
+                
+                print(f"Úkol číslo {vyber_cisla} byl smazán")
+                print(f"Smazali jste: {odstraneny_index}.\n")
+                break
+            else:
+                print(f"V zozname sa nenachází úkol číslo {vyber_cisla}. Vyber číslo ze seznamu.")
+        except ValueError:
+            print("Zadej číselný vstup!")
+            
+    hlavni_menu()    
 
-#   hlavni_menu()    
-
-#volba 4 v hlavnim menu
-#program se ukončí
+# ok    zadanie 4 - konec
+# ok    volba 4 v hlavnim menu
+# ok    program se ukončí
 
 def konec_programu():
     print("\nKonec programu.")
@@ -104,7 +99,7 @@ def konec_programu():
 # ok    neplatnou volbu, program ho upozorní a nechá uživatele opakovat znovu volbu.
 
 def hlavni_menu():
-    print("Správce úkolů - Hlavní menu")
+    print("\nSprávce úkolů - Hlavní menu")
     print("1. Přidat nový úkol")
     print("2. Zobrazit všechny úkoly")
     print("3. Odstranit úkol")
@@ -114,13 +109,13 @@ def hlavni_menu():
         vyber_cisla=int(input("Vyberte možnost (1-4):"))
         print(vyber_cisla)
         if vyber_cisla == 1:
-            print("Přidání nového úkolu")
+            print("\nPřidání nového úkolu")
             pridat_ukol()
         elif vyber_cisla == 2:
-            print("Zobrazení všech úkolů")
+            print("\nZobrazení všech úkolů")
             zobrazit_ukoly()
         elif vyber_cisla == 3:
-            print("Odstranění úkolu")
+            print("\nOdstranění úkolu")
             odstranit_ukol()
         elif vyber_cisla == 4:
             konec_programu()
