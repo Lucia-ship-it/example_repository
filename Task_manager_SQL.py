0e86cd61c282d029c4a8f1ac9c175cb40bfce00f
ella <elalek@gmail.com>
2025-07-03 16:58:07 +0200

    ripojenie

---
+++ Task_manager_SQL.py
@@ -0,0 +1,16 @@
+# import pymysql
+
+# try:
+#     conn = pymysql.connect(
+#             host="mysql80.r4.websupport.sk",
+#             port=3314,
+#             user="EsPMMROq",
+#             password = "79_|rBg[1F=`}cj|I%kc",
+#             database = "Task_manager_SQL"
+#             # server: mysql80.r4.websupport.sk:3314            
+#         )
+#     print("Připojení k databázi bylo úspěšné.")
+# except pymysql.connect.Error as err:
+#      print(f"Chyba při připojování: {err}")
+
+# cursor = conn.cursor()
