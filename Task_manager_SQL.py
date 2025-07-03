35e6647f9fa0b23236065e87c27ca4f7b6566fbc
ella <elalek@gmail.com>
2025-07-03 17:50:48 +0200

    ako spojazdnit pymysql cez venv

--- Task_manager_SQL.py
+++ Task_manager_SQL.py
@@ -1,16 +1,30 @@
-# import pymysql
+import pymysql
+
+try:
+    conn = pymysql.connect(
+            host="mysql80.r4.websupport.sk",
+            port=3314,
+            user="EsPMMROq",
+            password="79_|rBg[1F=`}cj|I%kc",
+            database="Task_manager_SQL"            
+        )
+    print("Připojení k databázi bylo úspěšné.")
+except pymysql.connect.Error as err:
+     print(f"Chyba při připojování: {err}")
+
+cursor = conn.cursor()
 
 # try:
-#     conn = pymysql.connect(
-#             host="mysql80.r4.websupport.sk",
-#             port=3314,
-#             user="EsPMMROq",
-#             password = "79_|rBg[1F=`}cj|I%kc",
-#             database = "Task_manager_SQL"
-#             # server: mysql80.r4.websupport.sk:3314            
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS Produkty (
+#         id INT PRIMARY KEY AUTO_INCREMENT,
+#         name VARCHAR(200) NOT NULL,
+#         price DECIMAL(10,2),
+#         in_storage INT
 #         )
-#     print("Připojení k databázi bylo úspěšné.")
-# except pymysql.connect.Error as err:
-#      print(f"Chyba při připojování: {err}")
+#     ''')
+#     print("Tabulka 'Produkty' byla vytvořena.")
 
-# cursor = conn.cursor()
+# except pymysql.connector.Error as err:
+#     print(f"Chyba při vytváření tabulky: {err}")
+# print("Připojení k databázi bylo uzavřeno.")
