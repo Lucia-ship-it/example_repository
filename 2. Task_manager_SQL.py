762601e9b7019ed301b2c9f851b2de29645e1c08
ella <elalek@gmail.com>
2025-07-06 11:14:01 +0200

    pridanie zadani a premyslanie o funkciach

--- 2. Task_manager_SQL.py
+++ 2. Task_manager_SQL.py
@@ -1,6 +1,18 @@
 import pymysql
 from datetime import date
 
+# zadanie
+# Použití MySQL databáze: Vytvoříte databázovou tabulku ukoly, která bude obsahovat: 
+# - id 
+# - nazev
+# - popis
+# - stav (nezahájeno, hotovo, probíhá)
+# - datum vytvoreni
+
+# ---------- Funkce programu ----------
+# 1. pripojeni_db() – Připojení k databázi
+# OK Funkce vytvoří připojení k MySQL databázi.
+# OK Pokud připojení selže, zobrazí chybovou zprávu.
 def pripojeni_db():
     try:
         conn = pymysql.connect(
@@ -15,8 +27,11 @@ def pripojeni_db():
     except pymysql.MySQLError as err:
         print(f"Chyba při připojování: {err}")
 
+# 2. vytvoreni_tabulky() – Vytvoření tabulky, pokud neexistuje
+# OK - Funkce vytvoří tabulku ukoly, pokud ještě neexistuje.
+# OK - Ověří existenci tabulky v databázi.
 
-def vytvoreni_tabulky(conn): # ak este neexistuje
+def vytvoreni_tabulky(conn):
     try:
         cursor = conn.cursor()
         cursor.execute('''
@@ -24,7 +39,7 @@ def vytvoreni_tabulky(conn): # ak este neexistuje
             id INT PRIMARY KEY AUTO_INCREMENT,
             nazev VARCHAR (50) NOT NULL,
             popis VARCHAR (255) NOT NULL,
-            stav  ENUM ('nezahájeno', 'hotovo', 'probíhá') NOT NULL DEFAULT 'nezahájeno', 
+            stav  ENUM ('nezahájeno', 'hotovo', 'probíhá') NOT NULL, 
             datum_vytvoreni DATE
             )
         ''')
@@ -38,7 +53,10 @@ def vytvoreni_tabulky(conn): # ak este neexistuje
 conn = pripojeni_db()
 if conn:
     vytvoreni_tabulky(conn)
+    print("Tabulka 'Ukoly' je k dispozici.")
     conn.close()
+
+
 
 # v dalsej funkcii osetrit vstupy na stav ENUM 'nezahájeno', 'hotovo', 'probíhá'
 #  pozor na chybz
@@ -46,4 +64,56 @@ if conn:
 # Táto podmienka bude vždy pravdivá, pretože vstup sa nikdy nemôže rovnať všetkým trom naraz. Treba to prepísať.
 # urobit premennu a moznosti ako list.
 
+# 3. hlavni_menu() – Hlavní nabídka
+#  - Zobrazí možnosti:
+#     1. Přidat úkol
+#     2. Zobrazit úkoly
+#     3. Aktualizovat úkol
+#     4. Odstranit úkol
+#     5. Ukončit program
+# - Pokud uživatel zadá špatnou volbu, program ho upozorní a nechá ho vybrat znovu.
 
+# 4. pridat_ukol() – Přidání úkolu
+#   - Uživatel zadá název a popis úkolu.
+#  - Povinné údaje: Název i popis jsou povinné, nesmí být prázdné.
+# - Automatické hodnoty:
+#     1. Úkol dostane ID automaticky.
+#     2. Výchozí stav ukolu: Nezahájeno
+# - Po splnění všech podmínek se úkol uloží do databáze
+
+nazev_ukolu = input(“Zadejte název úkolu”)
+popis_ukolu = input(“Zadejte popis úkolu”)
+cursor.execute(“INSERT INTO Ukoly (nazov, popis, stav) VALUES %s,%s,%s, (nazev, popis, stav)
+
+def osetrenie_vstupu_pre_stav():
+    stav_dovolene_vstupy = ['nezahájeno', 'hotovo', 'probíhá']
+    stav_ukolu = input("Napište, v jakém stavu je váš úkol. Možnosti: nezahájeno, hotovo, probíhá.").lower().strip()
+    if stav in stav_dovolene_vstupy:
+        try:
+            #funkcia pre zapisanie stavu do tabulky.
+        except:
+        print("Zadej správný výběr stavu úkolu")
+        return
+# 5. zobrazit_ukoly() – Zobrazení úkolů
+#   - Seznam všech úkolů s informacemi: ID, název, popis, stav.
+#  - Filtr: Zobrazí pouze úkoly se stavem "Nezahájeno" nebo "Probíhá".
+# - Pokud nejsou žádné úkoly, zobrazí informaci, že seznam je prázdný.
+def zobrat_ukoly():
+    cursor.execute(“SELECT id, name, stav FROM UKOLY”)
+    ukoly = cursor. fetchall()
+    return ukoly
+    cursor.close()
+
+
+# 6. aktualizovat_ukol() – Změna stavu úkolu
+# - Uživatel vidí seznam úkolů (ID, název, stav).
+# - Vybere úkol podle ID.
+# - Dostane na výběr nový stav: "Probíhá" nebo "Hotovo"
+# - Po potvrzení se aktualizuje DB.
+# -  Pokud zadá neexistující ID, program ho upozorní a nechá ho vybrat znovu.
+
+# 7. odstranit_ukol() – Odstranění úkolu
+# - Uživatel vidí seznam úkolů.
+# -  Vybere úkol podle ID.
+# - Po potvrzení bude úkol trvale odstraněn z databáze.
+# - Pokud uživatel zadá neexistující ID, program ho upozorní a nechá ho vybrat znovu.
