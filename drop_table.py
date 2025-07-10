94d6cefdccd9df4eb20a72d131d2cdd5be9af809
Lucia-ship-it <lucia.kobzova@icloud.com>
2025-07-10 09:10:54 +0200

    vymayanie celej tabulky

---
+++ drop_table.py
@@ -0,0 +1,13 @@
+import pymysql
+
+conn = pymysql.connect(
+        host="mysql80.r4.websupport.sk",
+        port=3314,
+        user="EsPMMROq",
+        password="79_|rBg[1F=`}cj|I%kc",
+        database="Task_manager_SQL#"            
+    )
+
+cursor=conn.cursor()
+#cursor.execute("" \
+#"DROP TABLE Ukoly;")
